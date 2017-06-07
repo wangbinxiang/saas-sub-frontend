6e4a439f24b52c20064f6a5bbb915323ba2278c8
wangbinxiang <45696104@qq.com>
2017-06-07 17:09:14 +0800

    v0.7.10, produc翻译层修改

--- server-src/libs/RequestAdapter.js
+++ server-src/libs/RequestAdapter.js
@@ -1,4 +1,4 @@
-import RequestJsonApiParamsError from './error/RequestJsonApiParamsError';
+import RequestJsonApiParamsError from './error/RequestJsonApiParamsError'
 
 /**
  * 适配模式基类
@@ -10,27 +10,28 @@ export default class RequestAdapter {
      * @date   2016-09-27T16:55:13+0800
      * @return {[type]}                 [description]
      */
-    async request() {
-        let result = null;
+  async request () {
+    let result = null
 
-        const { header, body } = await this.requestObject.request();
-        if (header.statusCode === this.requestObject.getSuccessCode()) {
-            result = this.translator.toObject(body, this.activeClass);
-        } else if (header.statusCode === this.requestObject.noContentCode) {
-            result = null;
-        } else if (header.statusCode === this.requestObject.getParamsErrorCode()) {
-            throw new RequestJsonApiParamsError('request JsonApi no params Error');
-        } else {
-            throw new Error('Invalid status');
-        }
-
-        return result;
+    const { header, body } = await this.requestObject.request()
+    if (header.statusCode === this.requestObject.getSuccessCode()) {
+      result = this.translator.toObject(body, this.activeClass)
+    } else if (header.statusCode === this.requestObject.noContentCode) {
+      result = null
+    } else if (header.statusCode === this.requestObject.getParamsErrorCode()) {
+      result = null
+      // throw new RequestJsonApiParamsError('request JsonApi no params Error')
+    } else {
+      throw new Error('Invalid status')
     }
 
-    //如果idList是数组 则需要数组形式的结果
-    needArrayResult(idList) {
-        if (idList && idList instanceof Array) {
-            this.translator.needArrayResult();
-        }
+    return result
+  }
+
+    // 如果idList是数组 则需要数组形式的结果
+  needArrayResult (idList) {
+    if (idList && idList instanceof Array) {
+      this.translator.needArrayResult()
     }
+  }
 }
--- server-src/models/application/ProductService.js
+++ server-src/models/application/ProductService.js
@@ -1,145 +1,144 @@
-import ProductAdapter from '../adapter/ProductAdapter';
-import ProductTypeAdapter from '../adapter/ProductTypeAdapter';
-import Product from '../model/Product';
-import ProductType from '../model/ProductType';
-import lodash from 'lodash';
-import { checkResourcesOwner, checkOther } from '../../libs/helper';
-import { PRODUCT_STATUS_ON_SALE } from '../../config/productConf'
+import ProductAdapter from '../adapter/ProductAdapter'
+import ProductTypeAdapter from '../adapter/ProductTypeAdapter'
+import Product from '../model/Product'
+import ProductType from '../model/ProductType'
+import lodash from 'lodash'
+import {
+  checkResourcesOwner,
+  checkOther
+} from '../../libs/helper'
+import {
+  PRODUCT_STATUS_ON_SALE
+} from '../../config/productConf'
 
 export default class ProductService {
+  constructor () {
+    this.productAdapter = new ProductAdapter()
+  }
 
-	constructor() {
-		this.productAdapter = new ProductAdapter();
-	}
+  async index (filters, pages) {
+    let result = await this.productAdapter.get({
+      filters,
+      pages
+    }, Product)
 
-	async index(filters, pages) {
-		let result = await this.productAdapter.get({
-			filters,
-			pages
-		}, Product);
+    if (result === null) {
+      // 没有获取数据 直接返回空
+      return null
+    } else {
+      let {
+        page,
+        result: products
+      } = result
 
-		if (result === null) {
-			//没有获取数据 直接返回空
-			return null;
-		} else {
+      let productTypeIdList = []
+      // let attachmentIdList = [];
+      // let logoList = []
+      for (let i in products) {
+        productTypeIdList.push(products[i].productTypeId)
+        // attachmentIdList.push(products[i].logoId);
+      }
 
-			let { page, result: products } = result;
-			
+      // 获取productType信息
+      if (productTypeIdList && productTypeIdList.length > 0) {
+        // try {
 
-			let productTypeIdList = []
-			// let attachmentIdList = [];
-			// let logoList = []
-			for (let i in products) {
-				productTypeIdList.push(products[i].productTypeId);
-				// attachmentIdList.push(products[i].logoId);
-			}
+        productTypeIdList = lodash.uniq(productTypeIdList)
+        const productTypeAdapter = new ProductTypeAdapter()
+        let productTypes = await productTypeAdapter.get({
+          'idList': productTypeIdList
+        }, ProductType)
 
-			//获取productType信息
-			if (productTypeIdList && productTypeIdList.length > 0) {
-				// try {
+        if (productTypes !== null) {
+          if (productTypes) {
+            for (let i in products) {
+              products[i].productType = productTypes[products[i].productTypeId]
+            }
+          }
+        }
+      }
 
-				productTypeIdList = lodash.uniq(productTypeIdList);
-				const productTypeAdapter = new ProductTypeAdapter();
-				let productTypes = await productTypeAdapter.get({
-					'idList': productTypeIdList
-				}, ProductType);
+      // 返回 分页 和 Products 数据
+      return {
+        page,
+        products
+      }
+    }
+  }
 
+  async list (idList) {
+    let products = await this.productAdapter.get({
+      idList
+    }, Product)
 
-				if (productTypes !== null) {
-					if (productTypes) {
-						for (let i in products) {
-							products[i].productType = productTypes[products[i].productTypeId];
-						}
-					}
-				}
-			}
+    if (products) {
+      products = lodash.remove(products, function (product) {
+        return product.status === PRODUCT_STATUS_ON_SALE
+      })
+    }
 
-			//返回 分页 和 Products 数据
-			return {
-				page,
-				products
-			};
-		}
-	}
+    return products
+  }
 
-	async list(idList) {
-		let products = await this.productAdapter.get({
-			idList
-		}, Product);
+  async get (idList, userId) {
+    const include = ['slides', 'description', 'prices']
+    let product = await this.productAdapter.get({
+      idList,
+      include
+    }, Product)
+    console.log(product)
+    if (product === null || (!checkOther(idList, userId) && !checkResourcesOwner(product, 'userId', userId, lodash.isArray(idList)))) {
+      return null
+    } else {
+      let productTypeId = product.productTypeId
 
-		if(products) {
-			products = lodash.remove(products, function(product) {
-				return product.status === PRODUCT_STATUS_ON_SALE
-			});
-		}
+      if (productTypeId) {
+        const productTypeAdapter = new ProductTypeAdapter()
+        let productType = await productTypeAdapter.get({
+          'idList': productTypeId
+        }, ProductType)
 
-		return products;
-	}
+        if (productType !== null) {
+          product.productType = productType
+        }
+      }
 
+      return product
+    }
+  }
 
-	async get(idList, userId) {
-		const include = ['slides', 'description', 'prices']
-		let product = await this.productAdapter.get({
-			idList,
-			include
-		}, Product);
-		;
-		if (product === null || (!checkOther(idList, userId) && !checkResourcesOwner(product, 'userId', userId, lodash.isArray(idList)))) {
-			return null;
-		} else {
+  add (userId, name, feature, productType, description, slides) {
+    return this.productAdapter.add({
+      userId,
+      name,
+      feature,
+      productType,
+      description,
+      slides
+    }, Product)
+  }
 
-			let productTypeId = product.productTypeId;
+  edit (id, name, feature, productType, description, slides) {
+    return this.productAdapter.edit({
+      id,
+      name,
+      feature,
+      productType,
+      description,
+      slides
+    }, Product)
+  }
 
-			if (productTypeId) {
-				const productTypeAdapter = new ProductTypeAdapter();
-				let productType = await productTypeAdapter.get({
-					'idList': productTypeId
-				}, ProductType);
+  del (id) {
+    return this.productAdapter.del({
+      id
+    }, Product)
+  }
 
-				if (productType !== null) {
-					product.productType = productType;	
-				}
-			}
-
-			return product;
-		}
-	}
-
-
-	add(userId, name, feature, productType, description, slides) {
-		return this.productAdapter.add({
-			userId,
-			name,
-			feature,
-			productType,
-			description,
-			slides
-		}, Product);
-	}
-
-	edit(id, name, feature, productType, description, slides) {
-		return this.productAdapter.edit({
-			id,
-			name,
-			feature,
-			productType,
-			description,
-			slides
-		}, Product);
-	}
-
-	del(id) {
-		return this.productAdapter.del({
-			id
-		}, Product);
-	}
-
-	addPrices(id, prices) {
-		return this.productAdapter.addPrices({
-			id,
-			prices
-		}, Product);
-
-	}
-
+  addPrices (id, prices) {
+    return this.productAdapter.addPrices({
+      id,
+      prices
+    }, Product)
+  }
 }
